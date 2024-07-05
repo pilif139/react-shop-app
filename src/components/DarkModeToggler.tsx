2184032203be8892da6b0fc64b11147b2839266c
pilif139 <pifilif@gmail.com>
2024-07-05 13:43:11 +0200

    fixed bug with dark mode toggle

--- src/components/DarkModeToggler.tsx
+++ src/components/DarkModeToggler.tsx
@@ -2,25 +2,43 @@ import { FaMoon } from "react-icons/fa";
 import { FaSun } from "react-icons/fa";
 import {useEffect, useState} from "react";
 
+type Theme = 'light' | 'dark';
+
 function DarkModeToggler() {
-  const [theme, setTheme] = useState<string>(localStorage.theme);
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    if(theme==="light"){
+        setTheme("dark");
+        window.localStorage.setItem('theme', 'dark');
+        document.documentElement.classList.add('dark');
+    }
+    else{
+        setTheme("light");
+        window.localStorage.setItem('theme', 'light');
+        document.documentElement.classList.remove('dark');
+    }
+  }
 
   useEffect(() => {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
+    const localTheme = window.localStorage.getItem('theme') as Theme | null;
+    if(localTheme){
+      setTheme(localTheme);
+      if(localTheme === 'dark'){
+        document.documentElement.classList.add('dark');
+      }
     }
-    localStorage.theme = theme;
-  },[theme])
+    else if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      setTheme('dark');
+        document.documentElement.classList.add('dark');
+    }
+  },[])
 
   return (
     <button className="transition-colors dark:bg-gray-900 bg-indigo-500 text-white p-5 rounded-3xl absolute bottom-7 right-7 scale-[1.12]"
-      onClick={() => {
-        setTheme(theme === 'dark' ? 'light' : 'dark');
-      }}
+      onClick={toggleTheme}
     >
-      {theme === 'dark' ? <FaSun /> : <FaMoon/>}
+      {theme === 'dark' ? <FaMoon/> : <FaSun />}
     </button>
   );
 }
